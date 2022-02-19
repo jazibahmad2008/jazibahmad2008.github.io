aed191c8d60fedc1e66ffdccbeaf14d38bf0111b
jazibahmad2008 <amir_n18@hotmail.com>
2022-02-19 16:50:27 -0600

    New Project

---
+++ randomcolorgenerator/script.js
@@ -0,0 +1,26 @@
+let currentColor = document.querySelector('.current-color');
+let bodyElem = document.querySelector('body');
+let generateBtn = document.querySelector('.generate-btn');
+
+bodyElem.style.backgroundColor = currentColor.innerText;
+
+generateBtn.addEventListener('click', ()=> {
+    let randomColor = "";
+    let charactors = "0123456789abcdef";
+
+    /* THE FOR LOOP IS VERY USEFUL IN JAVASCRIPT SYNTAX:
+    FOR (INITIALISER; CONDITION; INCREMENT) {
+        CODE
+    }
+*/
+
+for(let i = 0; i < 6; i++) {
+    randomColor = randomColor + charactors[Math.floor(Math.random() * 16)];
+}
+
+currentColor.innerText = "#" + randomColor;
+bodyElem.style.backgroundColor = "#" + randomColor;
+})
+
+
+        
